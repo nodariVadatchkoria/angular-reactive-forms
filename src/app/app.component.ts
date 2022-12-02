db6901ee4c71381e336dab3da731cee0e4b1596e
nodar vadatchkoria <nodar.vadachkoria@gmail.com>
2022-12-03 03:55:09 +0400

    CV

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,88 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reactive-forms';
+
+  form: FormGroup = new FormGroup({
+    firstname: new FormControl('',
+      Validators.required),
+    lastname: new FormControl('',
+      Validators.required),
+    userContactInfo: new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      phone: new FormControl('', [
+        Validators.required,
+        Validators.minLength(9),
+        Validators.maxLength(12),
+        Validators.pattern('[0-9]+')
+      ]),
+      socialMedia: new FormControl('',[
+        Validators.required,
+      ]),
+      link: new FormControl('', [
+        Validators.required,
+        Validators.pattern('https?://.+')
+      ]),
+      active: new FormControl('true',
+        Validators.required
+        ),
+      description: new FormControl('',
+        Validators.required),
+    }),
+    experience: new FormArray([
+      new FormGroup({
+        company: new FormControl('',
+          Validators.required,
+        ),
+        years: new FormControl('',
+          Validators.required,
+        ),
+      }),
+    ])
+  });
+
+  get experienceFormArray() {
+    return this.form.get('experience') as FormArray;
+  }
+
+  addExperience() {
+    this.experienceFormArray.push(new FormGroup({
+      company: new FormControl('',
+      Validators.required),
+      years: new FormControl('',[
+        Validators.required,
+        Validators.pattern('[0-9]+'),
+        Validators.min(1),
+        Validators.max(99)
+      ]),
+      })
+    )}
+
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  submit() {
+    this.form.markAllAsTouched();
+    console.log(this.form);
+
+  }
+
+  removeExperience(i: number) {
+    this.experienceFormArray.removeAt(i);
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterLink, RouterOutlet} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterLink,
+    RouterOutlet
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
